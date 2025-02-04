f8947f917180709eb049198c00940df056a38f95
Bachir Seghir <bachir@BMacM.local>
2025-02-05 00:44:41 +0100

    added updateStudent action

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -214,6 +214,9 @@ export const deleteTeacher = async (currentState: CurrentState, data: FormData) 
 
     const id = data.get("id") as string
     try {
+        // delete user from clerk
+        const client = await clerkClient()
+        await client.users.deleteUser(id)
 
         // Delete related assignments, attendances, and exams linked to lessons of this teacher
         await prisma.$transaction(async (prisma) => {
@@ -267,10 +270,120 @@ export const createStudent = async (currentState: CurrentState, data: StudentSch
         if (classItem?.capacity === classItem?._count.students) {
             return { success: false, error: true }
         }
+
+        const client = await clerkClient()
+        const user = await client.users.createUser({
+            username: data.username,
+            password: data.password,
+            firstName: data.firstName,
+            lastName: data.lastName,
+            publicMetadata: {
+                role: "student"
+            }
+        })
+        await prisma.student.create({
+            data: {
+                id: user.id,
+                username: data.username,
+                firstName: data.firstName,
+                lastName: data.lastName,
+                email: data.email,
+                phone: data.phone,
+                address: data.address,
+                img: data.img,
+                bloodType: data.bloodType,
+                sex: data.sex,
+                birthday: data.birthday,
+                gradeId: data.gradeId,
+                classId: data.classId,
+                parentId: data.parentId,
+            }
+        })
+
         return { success: true, error: false }
 
     } catch (error) {
         console.log(error);
         return { success: false, error: true }
     }
+}
+
+export const updateStudent = async (currentState: CurrentState, data: StudentSchema) => {
+
+    if (!data.id) {
+        return { success: false, error: true }
+    }
+    try {
+        const client = await clerkClient()
+        const user = await client.users.updateUser(data.id, {
+            username: data.username,
+            ...(data.password !== "" && { password: data.password }),
+            firstName: data.firstName,
+            lastName: data.lastName,
+        })
+        await prisma.student.update({
+            where: { id: data.id },
+            data: {
+                username: data.username,
+                firstName: data.firstName,
+                lastName: data.lastName,
+                email: data.email,
+                phone: data.phone,
+                address: data.address,
+                img: data.img,
+                bloodType: data.bloodType,
+                sex: data.sex,
+                birthday: data.birthday,
+                gradeId: data.gradeId,
+                classId: data.classId,
+                parentId: data.parentId,
+            }
+        })
+
+        return { success: true, error: false }
+
+    } catch (error) {
+        console.log(error);
+        return { success: false, error: true }
+    }
+
+}
+export const deleteStudent = async (currentState: CurrentState, data: FormData) => {
+
+    const id = data.get("id") as string
+    try {
+        // delete user from clerk
+        const client = await clerkClient()
+        await client.users.deleteUser(id)
+
+        // Delete related assignments, attendances, and exams linked to lessons of this teacher
+        // await prisma.$transaction(async (prisma) => {
+        //     await prisma.assignment.deleteMany({
+        //         where: { lesson: { teacherId: id } }
+        //     });
+        //     await prisma.attendance.deleteMany({
+        //         where: { lesson: { teacherId: id } }
+        //     });
+        //     await prisma.exam.deleteMany({
+        //         where: { lesson: { teacherId: id } }
+        //     });
+
+        //     // delete lesson associated with this teacher 
+        //     await prisma.lesson.deleteMany({
+        //         where: { teacherId: id }
+        //     })
+        // })
+
+        // delete the teacher
+        await prisma.student.delete({
+            where: { id }
+        })
+
+        return { success: true, error: false }
+
+    } catch (error) {
+        console.log(error);
+        return { success: false, error: true }
+    }
+
 }
